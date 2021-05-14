570ed950c639da2d191fce7b4eb48f81ec9330ab
ChristophorusX <mrxrtxrt@gmail.com>
2021-05-14 15:08:02 -0400

    more freq

--- simulation_align.py
+++ simulation_align.py
@@ -14,11 +14,11 @@ rc('text', usetex=True)
 n, d = (50, 150)
 step = 10e-7
 n_step = 6000
-n_iter = 10
+n_iter = 20
 activation = 'relu'
 p_start = 300
 p_end = 1000
-p_step = 100
+p_step = 50
 p_list = np.arange(start=p_start, stop=p_end + p_step, step=p_step)
 reg_list = [0, 50, 100]
 
