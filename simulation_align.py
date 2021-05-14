eddc8c8b7045835ef995aeb387bc38301ca55fa1
ChristophorusX <mrxrtxrt@gmail.com>
2021-05-14 15:45:06 -0400

    linear

--- simulation_align.py
+++ simulation_align.py
@@ -11,27 +11,18 @@ plt.style.use('ggplot')
 # rc('font', **{'family': 'serif', 'serif': ['Latin Modern Roman']})
 rc('text', usetex=True)
 
-n, d = (50, 150)
-step = 10e-7
-n_step = 6000
-n_iter = 20
-activation = 'relu'
-p_start = 300
-p_end = 1000
-p_step = 50
-p_list = np.arange(start=p_start, stop=p_end + p_step, step=p_step)
-reg_list = [0, 50, 100]
 
-
-def get_align_df(p_list, reg_list, activation):
+def get_align_df(n, d, p_list, reg_list, activation, synthetic_data, step, n_step, n_iter):
     reg_align_df = []
     for reg in reg_list:
         align_table = []
         for p in p_list:
             align_array = []
             for iter in range(n_iter):
-                # X, y = data_gen.lr_data(n, d)
-                X, y = data_gen.rand_nn_data(n, d, p)
+                if synthetic_data == 'lr':
+                    X, y = data_gen.lr_data(n, d)
+                elif synthetic_data == 'nn':
+                    X, y = data_gen.rand_nn_data(n, d, p)
                 seed = np.random.randint(100000)
                 net = fa.TwoLayerNetwork(activation, d, p, n, seed)
                 loss_fa, beta, b = net.feedback_alignment(
@@ -49,18 +40,52 @@ def get_align_df(p_list, reg_list, activation):
             (p_index, flattened_table, reg_index, activation_index)).T
         align_df = pd.DataFrame(data=combined_table, columns=[
                                 r"$p$ Hidden Layer Width", 'Alignment', r"Regularization $\lambda$", 'Activation'])
-        align_df[r"$p$ Hidden Layer Width"] = align_df[r"$p$ Hidden Layer Width"].astype(int)
+        align_df[r"$p$ Hidden Layer Width"] = align_df[r"$p$ Hidden Layer Width"].astype(
+            int)
         align_df['Alignment'] = align_df['Alignment'].astype(np.double)
-        align_df[r"Regularization $\lambda$"] = align_df[r"Regularization $\lambda$"].astype(int)
+        align_df[r"Regularization $\lambda$"] = align_df[r"Regularization $\lambda$"].astype(
+            int)
         reg_align_df.append(align_df)
     df = pd.concat(reg_align_df)
     return df
 
 
-df_relu = get_align_df(p_list, reg_list, 'relu')
-df_relu
+def plot_relu(df_relu):
+    align_relu_plot = sns.lineplot(x=r"$p$ Hidden Layer Width", y='Alignment',
+                                   hue=r"Regularization $\lambda$", data=df_relu, legend="full")
+    align_relu_fig = align_relu_plot.get_figure()
+    align_relu_fig.savefig('align_relu_fig.pdf')
 
-align_relu_plot = sns.lineplot(x=r"$p$ Hidden Layer Width", y='Alignment',
-                               hue=r"Regularization $\lambda$", data=df_relu, legend="full")
-align_relu_fig = align_relu_plot.get_figure()
-align_relu_fig.savefig('align_relu_fig.pdf')
+
+def plot_lr(df_lr):
+    align_relu_plot = sns.lineplot(x=r"$p$ Hidden Layer Width", y='Alignment',
+                                   hue=r"Regularization $\lambda$", data=df_lr, legend="full")
+    align_relu_fig = align_relu_plot.get_figure()
+    align_relu_fig.savefig('align_lr_fig.pdf')
+
+
+# # Generate alignment plot for relu network and nn data
+# n, d = (50, 150)
+# step = 10e-7
+# n_step = 6000
+# n_iter = 20
+# p_start = 300
+# p_end = 1000
+# p_step = 50
+# p_list = np.arange(start=p_start, stop=p_end + p_step, step=p_step)
+# reg_list = [0, 50, 100]
+# df_relu = get_align_df(n, d, p_list, reg_list, 'relu', 'nn', step, n_step, n_iter)
+# plot_relu(df_relu)
+
+# Generate alignment plot for linear network and lr data
+n, d = (50, 150)
+step = 10e-7
+n_step = 6000
+n_iter = 20
+p_start = 300
+p_end = 1000
+p_step = 50
+p_list = np.arange(start=p_start, stop=p_end + p_step, step=p_step)
+reg_list = [0, 50, 100]
+df_lr = get_align_df(n, d, p_list, reg_list, 'non', 'lr', step, n_step, n_iter)
+plot_lr(df_lr)
