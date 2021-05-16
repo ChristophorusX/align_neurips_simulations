ae05164fb91f4c5b7f497b49be9a8114ed749e71
Chris Xu <rx43@c20n10.grace.hpc.yale.internal>
2021-05-16 17:00:51 -0400

    from Grace

--- simulation_align.py
+++ simulation_align.py
@@ -175,6 +175,6 @@ p_start = 500
 p_end = 600
 p_step = 100
 p_list = np.arange(start=p_start, stop=p_end + p_step, step=p_step)
-reg_list = [0, 100]
+reg_list = [0, 500]
 df_relu = get_autograd_align_df(n, d, p_list, reg_list, 'relu', 'nn', step, n_step, n_iter)
 plot_autograd_relu(df_relu)
