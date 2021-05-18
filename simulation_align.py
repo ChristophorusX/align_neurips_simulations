049bb7017c3ddfb91966b781f43096d02fab64d3
Chris Xu <rx43@c21n02.grace.hpc.yale.internal>
2021-05-18 10:25:02 -0400

    from Grace

--- simulation_align.py
+++ simulation_align.py
@@ -179,21 +179,7 @@ def plot_autograd_lr(df_relu):
 #  plot_lr(df_lr)
 
 
-#  # Generate alignment plot for autograd relu network and nn data
-#  n, d = (50, 150)
-#  step = 10e-4
-#  n_step = 5000
-#  n_iter = 20
-#  p_start = 9000
-#  p_end = 10000
-#  p_step = 1000
-#  p_list = np.arange(start=p_start, stop=p_end + p_step, step=p_step)
-#  reg_list = [0, 2, 5]
-#  df_relu = get_autograd_align_df(n, d, p_list, reg_list, 'relu', 'nn', step, n_step, n_iter)
-#  plot_autograd_relu(df_relu)
-
-
-# Generate alignment plot for autograd linear network and lr data
+# Generate alignment plot for autograd relu network and nn data
 n, d = (50, 150)
 step = 10e-4
 n_step = 5000
@@ -203,6 +189,21 @@ p_end = 10000
 p_step = 100
 p_list = np.arange(start=p_start, stop=p_end + p_step, step=p_step)
 reg_list = [0, 1, 2]
-df_lr = get_autograd_align_df(n, d, p_list, reg_list, 'non', 'lr', step, n_step, n_iter)
-plot_autograd_lr(df_lr)
-df_lr.to_csv('df_lr_large.csv', index=False)
+df_relu = get_autograd_align_df(n, d, p_list, reg_list, 'relu', 'nn', step, n_step, n_iter)
+plot_autograd_relu(df_relu)
+df_relu.to_csv('df_relu_large.csv', index=False)
+
+
+#  # Generate alignment plot for autograd linear network and lr data
+#  n, d = (50, 150)
+#  step = 10e-4
+#  n_step = 5000
+#  n_iter = 10
+#  p_start = 5000
+#  p_end = 10000
+#  p_step = 100
+#  p_list = np.arange(start=p_start, stop=p_end + p_step, step=p_step)
+#  reg_list = [0, 1, 2]
+#  df_lr = get_autograd_align_df(n, d, p_list, reg_list, 'non', 'lr', step, n_step, n_iter)
+#  plot_autograd_lr(df_lr)
+#  df_lr.to_csv('df_lr_large.csv', index=False)
