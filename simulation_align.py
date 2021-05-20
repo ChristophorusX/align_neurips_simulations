faeb5c6b26fd688505bdd8662047b88d3fee3598
ChristophorusX <mrxrtxrt@gmail.com>
2021-05-20 13:12:48 -0400

    to server

--- simulation_align.py
+++ simulation_align.py
@@ -181,19 +181,19 @@ def plot_align(df, filename):
 #  df_relu.to_csv('df_relu_large.csv', index=False)
 
 
- # Generate alignment plot for autograd sigmoid network and nn data
- n, d = (50, 150)
- step = 10e-4
- n_step = 5000
- n_iter = 10
- p_start = 5000
- p_end = 10000
- p_step = 100
- p_list = np.arange(start=p_start, stop=p_end + p_step, step=p_step)
- reg_list = [0, 0.1, 1]
- df_relu = get_autograd_align_df(n, d, p_list, reg_list, 'sigmoid', 'nn', step, n_step, n_iter)
- plot_align(df_relu, 'align_autograd_sigmoid_fig_large.pdf')
- df_relu.to_csv('df_sigmoid_large.csv', index=False)
+# Generate alignment plot for autograd sigmoid network and nn data
+n, d = (50, 150)
+step = 10e-4
+n_step = 5000
+n_iter = 10
+p_start = 5000
+p_end = 10000
+p_step = 100
+p_list = np.arange(start=p_start, stop=p_end + p_step, step=p_step)
+reg_list = [0, 0.1, 1]
+df_relu = get_autograd_align_df(n, d, p_list, reg_list, 'sigmoid', 'nn', step, n_step, n_iter)
+plot_align(df_relu, 'align_autograd_sigmoid_fig_large.pdf')
+df_relu.to_csv('df_sigmoid_large.csv', index=False)
 
 
 #  # Generate alignment plot for autograd linear network and lr data
