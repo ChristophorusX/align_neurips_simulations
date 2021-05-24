82283ed525c87e247779a8eebd1f7c12a1b1c09c
ChristophorusX <mrxrtxrt@gmail.com>
2021-05-24 14:32:11 -0400

    mnist

--- mnist.py
+++ mnist.py
@@ -23,16 +23,17 @@ class MNISTTwoLayerFeedbackAlignmentNetworkReLU(nn.Module):
         self.input_features = 784
         self.hidden_features = hidden_features
 
-        self.first_layer = fa_autograd.FeedbackAlignmentReLU(
-            self.input_features, self.hidden_features)
+        self.first_layer = fa_autograd.FeedbackAlignmentRegReLU(
+            self.input_features, self.hidden_features, regularization)
         self.second_layer = fa_autograd.RegLinear(
             self.hidden_features, 10, regularization)
-        self.prediction = Variable(torch.FloatTensor(1, 10), requires_grad=True)
+        self.prediction = Variable(
+            torch.FloatTensor(1, 10), requires_grad=True)
 
     def forward(self, X):
-        hidden=self.first_layer(X)
+        hidden = self.first_layer(X)
         # self.prediction = self.second_layer(hidden) / np.sqrt(self.hidden_features)
-        self.prediction=self.second_layer(
+        self.prediction = self.second_layer(
             hidden) / np.sqrt(self.hidden_features)
         return F.log_softmax(self.prediction, dim=1)
 
@@ -40,21 +41,25 @@ class MNISTTwoLayerFeedbackAlignmentNetworkReLU(nn.Module):
 class MNISTThreeLayerFeedbackAlignmentNetworkReLU(nn.Module):
     def __init__(self, hidden_features, regularization):
         super(MNISTThreeLayerFeedbackAlignmentNetworkReLU, self).__init__()
-        self.input_features=784
-        self.hidden_features=hidden_features
+        self.input_features = 784
+        self.hidden_features = hidden_features
 
-        self.first_layer=fa_autograd.FeedbackAlignmentReLU(
-            self.input_features, self.hidden_features)
-        self.second_layer=fa_autograd.FeedbackAlignmentReLU(
-            self.hidden_features, self.hidden_features)
-        self.third_layer=fa_autograd.RegLinear(
+        self.first_layer = fa_autograd.FeedbackAlignmentRegReLU(
+            self.input_features, self.hidden_features, regularization)
+        self.second_layer = fa_autograd.FeedbackAlignmentRegReLU(
+            self.hidden_features, self.hidden_features, regularization)
+        self.third_layer = fa_autograd.RegLinear(
             self.hidden_features, 10, regularization)
+        self.hidden2 = Variable(torch.FloatTensor(
+            1, self.hidden_features), requires_grad=True)
+        self.prediction = Variable(
+            torch.FloatTensor(1, 10), requires_grad=True)
 
     def forward(self, X):
-        hidden=self.first_layer(X)
-        hidden2=self.second_layer(hidden)
-        prediction=self.second_layer(hidden2) / self.hidden_features
-        return F.log_softmax(prediction, dim=1)
+        hidden = self.first_layer(X)
+        self.hidden2 = self.second_layer(hidden)
+        self.prediction = self.second_layer(self.hidden2) / self.hidden_features
+        return F.log_softmax(self.prediction, dim=1)
 
 
 def get_align_mnist():
@@ -62,16 +67,16 @@ def get_align_mnist():
 
 
 def train_epoch_fa(torch_net_fa, mnist_trainset, mnist_testset, align_array, loss_array, accuracy_array):
-    train_loader=torch.utils.data.DataLoader(mnist_trainset)
-    test_loader=torch.utils.data.DataLoader(mnist_testset)
-    optimizer_fa=torch.optim.SGD(torch_net_fa.parameters(), lr=10e-4)
+    train_loader = torch.utils.data.DataLoader(mnist_trainset)
+    test_loader = torch.utils.data.DataLoader(mnist_testset)
+    optimizer_fa = torch.optim.SGD(torch_net_fa.parameters(), lr=10e-4)
     for batch_idx, (data, target) in enumerate(train_loader):
         # torch_net_fa.train()
-        data=data.flatten().unsqueeze(0).to(device)
-        target=target.to(device)
+        data = data.flatten().unsqueeze(0).to(device)
+        target = target.to(device)
         optimizer_fa.zero_grad()
-        output=torch_net_fa(data)
-        loss=F.nll_loss(output, target)
+        output = torch_net_fa(data)
+        loss = F.nll_loss(output, target)
         print(torch_net_fa.prediction.grad)
         torch_net_fa.prediction.retain_grad()
         loss.backward()
@@ -79,76 +84,78 @@ def train_epoch_fa(torch_net_fa, mnist_trainset, mnist_testset, align_array, los
         optimizer_fa.step()
         for name, param in torch_net_fa.named_parameters():
             if name == 'second_layer.backprop_weight':
-                backprop_weight=param.data
+                backprop_weight = param.data
             if name == 'second_layer.weight':
-                second_layer_weight=param.data
-        error_signal=torch_net_fa.prediction.grad
-        delta_fa=error_signal.mm(backprop_weight)
-        delta_bp=error_signal.mm(second_layer_weight)
-        align=torch.tensordot(delta_fa, delta_bp) / \
+                second_layer_weight = param.data
+        error_signal = torch_net_fa.prediction.grad
+        delta_fa = error_signal.mm(backprop_weight)
+        delta_bp = error_signal.mm(second_layer_weight)
+        align = torch.tensordot(delta_fa, delta_bp) / \
             torch.norm(delta_fa) / torch.norm(delta_bp)
-        align=align.cpu().data.detach().numpy().flatten()
+        align = align.cpu().data.detach().numpy().flatten()
         align_array.append(align)
         if batch_idx % 1000 == 999:
             print(align)
             # torch_net_fa.eval()
-            test_loss=0
-            n_correct=0
+            test_loss = 0
+            n_correct = 0
             with torch.no_grad():
                 for data_test, target_test in test_loader:
-                    data_test=data_test.flatten().unsqueeze(0).to(device)
-                    target_test=target_test.to(device)
-                    output_test=torch_net_fa(data_test)
+                    data_test = data_test.flatten().unsqueeze(0).to(device)
+                    target_test = target_test.to(device)
+                    output_test = torch_net_fa(data_test)
                     test_loss += F.nll_loss(output_test,
                                             target_test, reduction='sum').item()
-                    pred_test=output_test.argmax(dim=1, keepdim=True)
+                    pred_test = output_test.argmax(dim=1, keepdim=True)
                     n_correct += pred_test.eq(
                         target_test.view_as(pred_test)).sum().item()
             test_loss /= len(test_loader.dataset)
-            accuracy=n_correct / len(test_loader.dataset)
+            accuracy = n_correct / len(test_loader.dataset)
             loss_array.append(test_loss)
             accuracy_array.append(accuracy)
             print(batch_idx, test_loss, accuracy)
 
 
-n_epochs=1
-transform=transforms.Compose(
+n_epochs = 1
+transform = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-mnist_trainset=datasets.MNIST(
+mnist_trainset = datasets.MNIST(
     root='./data', train=True, download=True, transform=transform)
-mnist_testset=datasets.MNIST(
+mnist_testset = datasets.MNIST(
     root='./data', train=False, download=True, transform=transform)
-torch_net_fa=MNISTTwoLayerFeedbackAlignmentNetworkReLU(1000, 0).to(device)
-torch_net_fa_reg=type(torch_net_fa)(1000, 0).to(device)
+torch_net_fa = MNISTTwoLayerFeedbackAlignmentNetworkReLU(1000, 0).to(device)
+torch_net_fa_reg = type(torch_net_fa)(1000, 0).to(device)
 torch_net_fa_reg.load_state_dict(torch_net_fa.state_dict())
 for name, param in torch_net_fa_reg.named_parameters():
     if name == 'second_layer.regularization':
         print('modifying regularization...')
         param.data.copy_(0.1 * torch.ones_like(param.data))
-align_array=[]
-loss_array=[]
-accuracy_array=[]
+align_array = []
+loss_array = []
+accuracy_array = []
 for epoch in range(n_epochs):
     train_epoch_fa(torch_net_fa, mnist_trainset, mnist_testset,
                    align_array, loss_array, accuracy_array)
 # align_plot = plt.plot(np.arange(n_epochs * len(mnist_trainset)), align_array, label='Two Layer ReLU')
 # accuracy_plot = plt.plot(np.arange(len(accuracy_array)), accuracy_array)
-align_array_reg=[]
-loss_array_reg=[]
-accuracy_array_reg=[]
+align_array_reg = []
+loss_array_reg = []
+accuracy_array_reg = []
 for epoch in range(n_epochs):
     train_epoch_fa(torch_net_fa_reg, mnist_trainset, mnist_testset,
                    align_array_reg, loss_array_reg, accuracy_array_reg)
-fig=plt.figure()
-ax1=plt.subplot(211)
+fig = plt.figure()
+ax1 = plt.subplot(211)
 ax1.plot(np.arange(n_epochs * len(mnist_trainset)), align_array,
          np.arange(n_epochs * len(mnist_trainset)), align_array_reg)
-ax2=plt.subplot(212)
+ax2 = plt.subplot(212)
 ax2.plot(np.arange(len(accuracy_array)), accuracy_array,
          np.arange(len(accuracy_array_reg)), accuracy_array_reg)
 fig.savefig('mnist_alignment_relu.pdf')
-align_plot = plt.plot(np.arange(n_epochs * len(mnist_trainset)), align_array, np.arange(n_epochs * len(mnist_trainset)), align_array_reg)
-accuracy_plot = plt.plot(np.arange(len(accuracy_array)), accuracy_array, np.arange(len(accuracy_array_reg)), accuracy_array_reg)
+align_plot = plt.plot(np.arange(n_epochs * len(mnist_trainset)), align_array,
+                      np.arange(n_epochs * len(mnist_trainset)), align_array_reg)
+accuracy_plot = plt.plot(np.arange(len(accuracy_array)), accuracy_array, np.arange(
+    len(accuracy_array_reg)), accuracy_array_reg)
 # plt.xlabel('# of samples')
 # plt.ylabel('Alignment')
 # plt.savefig('mnist_relu_alignment.pdf')
