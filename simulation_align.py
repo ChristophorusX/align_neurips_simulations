9043aca626ed025631713417b924b3b9ee12b510
ChristophorusX <mrxrtxrt@gmail.com>
2021-05-24 21:51:12 -0400

    to server

--- simulation_align.py
+++ simulation_align.py
@@ -302,12 +302,12 @@ if __name__ == '__main__':
         step = 10e-2
         n_step = 5000
         reg_step = 2000
-        n_iter = 3
+        n_iter = 15
         # p_start = 5000
         # p_end = 10000
         # p_step = 100
         # p_list = np.arange(start=p_start, stop=p_end + p_step, step=p_step)
-        p_list = [800, 1600]
+        p_list = [200, 400, 800, 1600, 3200, 6400, 12800]
         reg_list = [0, 0.5, 0.7, 0.9]
         df_sigmoid = get_autograd_align_df(
             n, d, p_list, reg_list, 'sigmoid', 'nn', step, n_step, reg_step, n_iter, dropout=True)
