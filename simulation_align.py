c74f7764b4a065460439b2081919c429e99501b0
ChristophorusX <mrxrtxrt@gmail.com>
2021-05-26 09:06:50 -0400

    to

--- simulation_align.py
+++ simulation_align.py
@@ -127,7 +127,7 @@ def get_autograd_align_df(n, d, p_list, reg_list, activation, synthetic_data, st
                 else:
                     continue_flag = False
                 t = 0
-                loss_record = 0
+                align_record = 1
                 while t < proportion_step or continue_flag:
                     if reg_flag is True and t >= reg_step - 1:
                         print("Stop regularization at step {}".format(t))
@@ -151,9 +151,9 @@ def get_autograd_align_df(n, d, p_list, reg_list, activation, synthetic_data, st
                             torch.norm(second_layer_weight)
                         align = align.cpu().data.detach().numpy().flatten()
                         print(t, loss.item(), align)
-                        if np.abs(loss.item() - loss_record) < 0.1:
+                        if np.abs(align - align_record) < 0.01:
                             continue_flag = False
-                        loss_record = loss.item()
+                        align_record = align
                     optimizer_fa.zero_grad()
                     loss.backward()
                     optimizer_fa.step()
