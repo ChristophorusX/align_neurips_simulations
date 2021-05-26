88340e300917146643d3e62a2574bf1926adcf69
ChristophorusX <mrxrtxrt@gmail.com>
2021-05-26 12:43:01 -0400

    to

--- simulation_align.py
+++ simulation_align.py
@@ -338,18 +338,19 @@ if __name__ == '__main__':
         step = 10e-4
         n_step = 5000
         reg_step = 0
-        n_iter = 15
+        n_iter = 2
         # p_start = 5000
         # p_end = 10000
         # p_step = 100
         # p_list = np.arange(start=p_start, stop=p_end + p_step, step=p_step)
         p_list = [200, 2400, 12800]
-        reg_list = [0, 0.4, 0.6, 0.8]
+        # reg_list = [0, 0.4, 0.6, 0.8]
+        reg_list = [0.4]
         df_relu = get_autograd_align_df(
             n, d, p_list, reg_list, 'relu', 'nn', step, n_step, reg_step, n_iter, dropout=True)
-        plot_align(df_relu, "outputs/align_{}_{}_{}_{}_v2.pdf".format(args.data,
+        plot_align(df_relu, "outputs/align_{}_{}_{}_{}_v3.pdf".format(args.data,
                    args.network, args.scheme, args.regularization), len(reg_list))
-        df_relu.to_csv("dataframes/df_{}_{}_{}_{}_v2.csv".format(args.data,
+        df_relu.to_csv("dataframes/df_{}_{}_{}_{}_v3.csv".format(args.data,
                      args.network, args.scheme, args.regularization), index=False)
 
 
@@ -360,18 +361,19 @@ if __name__ == '__main__':
         step = 10e-2
         n_step = 5000
         reg_step = 0
-        n_iter = 15
+        n_iter = 2
         # p_start = 5000
         # p_end = 10000
         # p_step = 100
         # p_list = np.arange(start=p_start, stop=p_end + p_step, step=p_step)
         p_list = [200, 2400, 12800]
-        reg_list = [0, 0.5, 0.7, 0.9]
+        # reg_list = [0, 0.5, 0.7, 0.9]
+        reg_list = [0.5]
         df_sigmoid = get_autograd_align_df(
             n, d, p_list, reg_list, 'sigmoid', 'nn', step, n_step, reg_step, n_iter, dropout=True)
-        plot_align(df_sigmoid, "outputs/align_{}_{}_{}_{}_v2.pdf".format(args.data,
+        plot_align(df_sigmoid, "outputs/align_{}_{}_{}_{}_v3.pdf".format(args.data,
                    args.network, args.scheme, args.regularization), len(reg_list))
-        df_sigmoid.to_csv("dataframes/df_{}_{}_{}_{}_v2.csv".format(args.data,
+        df_sigmoid.to_csv("dataframes/df_{}_{}_{}_{}_v3.csv".format(args.data,
                      args.network, args.scheme, args.regularization), index=False)
 
     # Generate alignment plot for autograd tanh network and nn data with dropout
@@ -381,18 +383,19 @@ if __name__ == '__main__':
         step = 10e-2
         n_step = 5000
         reg_step = 0
-        n_iter = 15
+        n_iter = 2
         # p_start = 5000
         # p_end = 10000
         # p_step = 100
         # p_list = np.arange(start=p_start, stop=p_end + p_step, step=p_step)
         p_list = [200, 2400, 12800]
-        reg_list = [0, 0.5, 0.7, 0.9]
+        # reg_list = [0, 0.5, 0.7, 0.9]
+        reg_list = [0.5]
         df_sigmoid = get_autograd_align_df(
             n, d, p_list, reg_list, 'tanh', 'nn', step, n_step, reg_step, n_iter, dropout=True)
-        plot_align(df_sigmoid, "outputs/align_{}_{}_{}_{}_v2.pdf".format(args.data,
+        plot_align(df_sigmoid, "outputs/align_{}_{}_{}_{}_v3.pdf".format(args.data,
                    args.network, args.scheme, args.regularization), len(reg_list))
-        df_sigmoid.to_csv("dataframes/df_{}_{}_{}_{}_v2.csv".format(args.data,
+        df_sigmoid.to_csv("dataframes/df_{}_{}_{}_{}_v3.csv".format(args.data,
                      args.network, args.scheme, args.regularization), index=False)
 
 
@@ -409,10 +412,11 @@ if __name__ == '__main__':
         # p_step = 100
         # p_list = np.arange(start=p_start, stop=p_end + p_step, step=p_step)
         p_list = [200, 2400, 12800]
-        reg_list = [0, 0.1, 0.2, 0.3]
+        # reg_list = [0, 0.1, 0.2, 0.3]
+        reg_list = 0.1
         df_lr = get_autograd_align_df(
             n, d, p_list, reg_list, 'non', 'lr', step, n_step, reg_step, n_iter, dropout=True)
-        plot_align(df_lr, "outputs/align_{}_{}_{}_{}_v2.pdf".format(args.data,
+        plot_align(df_lr, "outputs/align_{}_{}_{}_{}_v3.pdf".format(args.data,
                    args.network, args.scheme, args.regularization), len(reg_list))
-        df_lr.to_csv("dataframes/df_{}_{}_{}_{}_v2.csv".format(args.data,
+        df_lr.to_csv("dataframes/df_{}_{}_{}_{}_v3.csv".format(args.data,
                      args.network, args.scheme, args.regularization), index=False)
