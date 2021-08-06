52afd69234e0d534f6bfdc7147b608df469c126e
ChristophorusX <mrxrtxrt@gmail.com>
2021-08-06 19:03:28 -0400

    debug

--- mnist.py
+++ mnist.py
@@ -371,7 +371,7 @@ if __name__ == '__main__':
     
     n_hidden = 1000
     lr = 1e-2
-    n_epochs = 3 #300
+    n_epochs = 30 # 300
     batch_size = 600
     n_layers = 2
     reg_levels = [0, 0.1, 0.3]
